6dc2f6e83a7f64afefba71bb83e6ee197c5cda30
liangdou <1368371032@qq.com>
2022-03-20 23:35:44 +0800

    人资项目初始化

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,6 +4,8 @@ import SvgIcon from '@/components/SvgIcon'// svg component
 // register globally
 Vue.component('svg-icon', SvgIcon)
 
+// 读取所有的.svg文件
 const req = require.context('./svg', false, /\.svg$/)
+// 将所有svg目录下的所有.svg后缀文件全部引入到项目中
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
