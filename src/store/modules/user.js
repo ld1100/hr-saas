3443edd1223edb96f732a3053385f1f660501d93
liangdou <1368371032@qq.com>
2022-03-27 22:12:50 +0800

    完成登录模块

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,97 +1,37 @@
-// import { login, logout, getInfo } from '@/api/user'
-// import { getToken, setToken, removeToken } from '@/utils/auth'
 // import { resetRouter } from '@/router'
-//
-// const getDefaultState = () => {
-//   return {
-//     token: getToken(),
-//     name: '',
-//     avatar: ''
-//   }
-// }
-//
-// const state = getDefaultState()
-//
-// const mutations = {
-//   RESET_STATE: (state) => {
-//     Object.assign(state, getDefaultState())
-//   },
-//   SET_TOKEN: (state, token) => {
-//     state.token = token
-//   },
-//   SET_NAME: (state, name) => {
-//     state.name = name
-//   },
-//   SET_AVATAR: (state, avatar) => {
-//     state.avatar = avatar
-//   }
-// }
-//
-// const actions = {
-//   // user login
-//   login({ commit }, userInfo) {
-//     const { username, password } = userInfo
-//     return new Promise((resolve, reject) => {
-//       login({ username: username.trim(), password: password }).then(response => {
-//         const { data } = response
-//         commit('SET_TOKEN', data.token)
-//         setToken(data.token)
-//         resolve()
-//       }).catch(error => {
-//         reject(error)
-//       })
-//     })
-//   },
-//
-//   // get user info
-//   getInfo({ commit, state }) {
-//     return new Promise((resolve, reject) => {
-//       getInfo(state.token).then(response => {
-//         const { data } = response
-//
-//         if (!data) {
-//           return reject('Verification failed, please Login again.')
-//         }
-//
-//         const { name, avatar } = data
-//
-//         commit('SET_NAME', name)
-//         commit('SET_AVATAR', avatar)
-//         resolve(data)
-//       }).catch(error => {
-//         reject(error)
-//       })
-//     })
-//   },
-//
-//   // user logout
-//   logout({ commit, state }) {
-//     return new Promise((resolve, reject) => {
-//       logout(state.token).then(() => {
-//         removeToken() // must remove  token  first
-//         resetRouter()
-//         commit('RESET_STATE')
-//         resolve()
-//       }).catch(error => {
-//         reject(error)
-//       })
-//     })
-//   },
-//
-//   // remove token
-//   resetToken({ commit }) {
-//     return new Promise(resolve => {
-//       removeToken() // must remove  token  first
-//       commit('RESET_STATE')
-//       resolve()
-//     })
-//   }
-// }
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login, logout, getInfo } from '@/api/user'
 
+// 状态
+const state = {
+  token: getToken() // 设置token初始状态   token持久化 => 放到缓存中
+}
+// 修改状态
+const mutations = {
+  //  设置token
+  setToken(state, token) {
+    state.token = token //  更新state中的token
+    setToken(token) //  将token同步给缓存
+  },
+  //  删除token
+  removeToken(state) {
+    state.token = null // 清空vuex中的token
+    removeToken() //  清空缓存中的token
+  }
+}
+// 执行异步
+const actions = {
+  //  定义登录action
+  async login(context, data) {
+    // 经过request中响应拦截器的处理之后 这里的result实际上就是 token
+    const result = await login(data)
+    context.commit('setToken', result)
+  }
+}
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
-  actions: {}
+  state,
+  mutations,
+  actions
 }
 
--- src/utils/request.js
+++ src/utils/request.js
@@ -86,7 +86,26 @@
 
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import axios from 'axios'
-const service = axios.create() // 创建一个axios的实例
+import { Message } from 'element-ui'
+// import {error} from "autoprefixer/lib/utils";
+const service = axios.create(
+  {
+    baseURL: process.env.VUE_APP_BASE_API, // 设置axios请求的基础地址，可通过.env.development或者.env.production文件进行设置
+    timeout: 5000
+  }
+) // 创建一个axios的实例
 service.interceptors.request.use() // 请求拦截器
-service.interceptors.response.use() // 响应拦截器
+service.interceptors.response.use(response => {
+  //  从response中解构出需要的数据
+  const { success, message, data } = response.data // axios中默认加了一层data
+  if (success) {
+    return data
+  } else { // success为false
+    Message.error(message) // 提示错误信息
+    return Promise.reject(new Error(message))
+  }
+}, error => {
+  Message.error(error.message) // 引入element中的 Message方法，提示错误信息
+  return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
+}) // 响应拦截器
 export default service // 导出axios实例
