a5bf92a0fc6790b9c25e895571cfcf71276ee5ad
liangdou <1368371032@qq.com>
2022-12-08 19:01:44 +0800

    feat(*):commit

--- src/main.js
+++ src/main.js
@@ -1,37 +1,34 @@
 import Vue from 'vue'
 
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
-
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
 import i18n from '@/Lang' // 引入i18n实例
 // import { i18n } from 'element-ui'
 import '@/styles/index.scss' // global css
 import '@/icons' // icon
 import '@/permission' // permission control
-
 import App from './App'
 import store from './store'
 import router from './router'
 import * as directives from '@/directives' // 引入自定义指令
+// 引入全局公共组件
+import Component from '@/components'
+import * as filters from '@/filters' // 引入工具类
+//打印插件
+import Print from 'vue-print-nb'
+
 Object.keys(directives).forEach(key => {
   // 遍历所有导出的自定义指令对象，完成自定义指令注册
   Vue.directive(key, directives[key])
 })
-// 引入全局公共组件
-import Component from '@/components'
 
 Vue.use(Component) // 注册自己的插件
-
-import * as filters from '@/filters' // 引入工具类
 // 注册全局的过滤器
 Object.keys(filters).forEach(key => {
   // 注册过滤器
   Vue.filter(key, filters[key])
 })
-//打印插件
-import Print from 'vue-print-nb'
 
 Vue.use(Print)
 /**
